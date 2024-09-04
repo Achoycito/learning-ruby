a5ff12ee7fc63322a6cc5d69e7e5d6c99f8d8534
AlanGonzalez <alanglez360@outlook.es>
2024-09-04 13:29:35 -0700

    Consultar propiedades en cuenta de Staging de EasyBroker
    Se conecta a la API de EasyBroker para consultar las propiedades en la cuenta de Staging y listar sus títulos

--- EasyBroker.rb
+++ EasyBroker.rb
@@ -1,14 +1,29 @@
-require 'uri'
-require 'net/http'
+require 'json'
+require 'rest-client'
 
-url = URI("https://api.easybroker.com/v1/properties")
+def printResults response
+  results = JSON.parse(response.to_str)
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
+  if results['pagination']['total'] > 0
+    puts "\n\n"
+    puts "Mostrando #{results['pagination']['total']} propiedades en la cuenta de Staging en la API de EasyBroker (pág. #{results['pagination']['page']})"
+  
+    results['content'].each.with_index 1 do |property, index|
+      puts "#{index}. #{property['title']}"
+    end
 
-request = Net::HTTP::Get.new(url)
-request["accept"] = 'application/json'
-request["X-Authorization"] = 'l7u502p8v46ba3ppgvj5y2aad50lb9'
+    puts "\n\n"
+  else
+    puts "No se encontraron propiedades pertenecientes a la API key proporcionada."
+  end
+end
 
-response = http.request(request)
-puts response.read_body
+url = "https://api.easybroker.com/v1/properties"
+auth = {"X-Authorization": "l7u502p8v46ba3ppgvj5y2aad50lb9"} # Token para cuenta de Staging
+
+begin
+  response = RestClient.get(url, headers=auth)
+  printResults response
+rescue RestClient::ExceptionWithResponse => e
+  puts e.response
+end
